477b5f586effef9f30ae7d557083d9c505980040
arvind <amahankali@stuy.edu>
2017-05-03 15:26:41 -0400

    found opcodes

--- my_main.c
+++ my_main.c
@@ -10,29 +10,40 @@
   Your job is to go through each entry in op and perform
   the required action from the list below:
 
-  push: push a new origin matrix onto the origin stack
-  pop: remove the top matrix on the origin stack
+  push: push a new origin matrix onto the origin stack (opcode = PUSH)
+  pop: remove the top matrix on the origin stack (opcode = POP)
 
-  move/scale/rotate: create a transformation matrix 
+  move/scale/rotate: create a transformation matrix (opcode = MOVE / SCALE / ROTATE)
                      based on the provided values, then 
 		     multiply the current top of the
 		     origins stack by it.
 
-  box/sphere/torus: create a solid object based on the
+  box/sphere/torus: create a solid object based on the (opcode = BOX / SPHERE / TORUS)
                     provided values. Store that in a 
 		    temporary matrix, multiply it by the
 		    current top of the origins stack, then
 		    call draw_polygons.
 
-  line: create a line based on the provided values. Store 
+  line: create a line based on the provided values. Store (opcode = LINE)
         that in a temporary matrix, multiply it by the
 	current top of the origins stack, then call draw_lines.
 
-  save: call save_extension with the provided filename
+  save: call save_extension with the provided filename (opcode = SAVE)
 
-  display: view the image live
+  display: view the image live (opcode = DISPLAY)
   
   jdyrlandweaver
+
+  %token COMMENT
+%token <val> DOUBLE
+%token <string> LIGHT AMBIENT
+%token <string> CONSTANTS SAVE_COORDS CAMERA 
+%token <string> SPHERE TORUS BOX LINE CS MESH TEXTURE
+%token <string> STRING
+%token <string> SET MOVE SCALE ROTATE BASENAME SAVE_KNOBS TWEEN FRAMES VARY 
+%token <string> PUSH POP SAVE GENERATE_RAYFILES
+%token <string> SHADING SHADING_TYPE SETKNOBS FOCAL DISPLAY WEB
+%token <string> CO
   =========================*/
 
 #include <stdio.h>
